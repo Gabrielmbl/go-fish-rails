eec16c220b7c967fa5df103d16090588aa215c90
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-07-09 14:10:36 -0400

    Added required number of players for game model, added input for number of players in simple form.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,6 @@
 class Game < ApplicationRecord
   validates :name, presence: true
+  validates :required_number_players, presence: true, numericality: { only_integer: true, greater_than: 1 }
 
   has_many :game_users, dependent: :destroy
   has_many :users, through: :game_users
