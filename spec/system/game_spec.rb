4ca4139b0955d9818c37f5006b5d84084393e5e0
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-07-09 09:01:44 -0400

    Added system tests for basic CRUD

--- spec/system/game_spec.rb
+++ spec/system/game_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe 'Games', type: :system do
     click_link game
 
     expect(page).not_to have_selector 'h1', text: game
+    expect(page).to have_text game.name
   end
 
   it 'creates a new game', :js do
@@ -24,15 +25,36 @@ RSpec.describe 'Games', type: :system do
 
     click_on 'New game'
 
-    # expect(page).to have_selector 'h1', text: 'Games'
-    # expect(page).to have_text 'Capybara game'
+    fill_in 'Name', with: 'Capybara game'
+    click_on 'Create Game'
+
+    expect(page).to have_text 'Game was successfully created.'
+    expect(page).to have_text 'Capybara game'
   end
 
   it 'destroys a game', :js do
     visit games_path
     expect(page).to have_text game.name
 
+    click_link game
+
     click_on 'Delete', match: :first
+    expect(page).to have_text 'Game was successfully destroyed.'
     expect(page).not_to have_text game.name
+  end
+
+  it 'updates a game', :js do
+    visit games_path
+    expect(page).to have_text game.name
+
+    click_link game
+
+    click_on 'Edit', match: :first
+
+    fill_in 'Name', with: 'Updated game'
+    click_on 'Update Game'
+
+    expect(page).to have_text 'Game was successfully updated.'
+    expect(page).to have_text 'Updated game'
   end
 end
