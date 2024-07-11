a3f3d3cc54f944926c3f6256ab5142e1f8244133
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-07-11 12:51:14 -0400

    Added #start and #go_fish tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,32 +5,42 @@ RSpec.describe Game, type: :model do
   let!(:user1) { create(:user) }
   let!(:user2) { create(:user) }
 
-  before do
-    create(:game_user, game:, user: user1)
-    create(:game_user, game:, user: user2)
-  end
-
-  # TODO: Test behaviors. Doesn't start game if there are not enough players
-  # if not, it should add players, and deals cards
-  # it has players, and players have more than 0 cards
   describe '#start!' do
-    it 'should update the database with the game started_at time' do
-      expect(game.go_fish).to be_nil
-      game.start!
+    context 'when there are not enough players' do
+      it 'should return false' do
+        expect(game.start!).to be_falsey
+      end
+    end
 
-      expect(game.go_fish).not_to be_nil
+    context 'when there are enough players' do
+      before do
+        create(:game_user, game:, user: user1)
+        create(:game_user, game:, user: user2)
+      end
+
+      it 'should populate the game with players' do
+        game.start!
+        expect(game.go_fish.players.size).not_to be_zero
+      end
+
+      it 'should initialize with correct players' do
+        game.start!
+        expect(game.go_fish.players.map(&:user_id)).to match_array([user1.id, user2.id])
+      end
+
+      it 'should deal cards to each player' do
+        game.start!
+        expect(game.go_fish.players.all? { |player| player.hand.size == GoFish::INITIAL_HAND_SIZE }).to be
+      end
     end
   end
 
   describe '#go_fish' do
-    it 'gives JSON form object' do
+    it 'returns a GoFish object' do
       players = [user1, user2].map { |user| Player.new(user_id: user.id) }
       go_fish = GoFish.new(players:)
       game.update(go_fish:)
-      expect(game.go_fish).not_to be_nil
-    end
-
-    xit 'gives object from JSON' do
+      expect(game.go_fish).to be_an_instance_of(GoFish)
     end
   end
 end
