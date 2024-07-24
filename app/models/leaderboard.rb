0a4cb2e99f80fc7ac3b4221245688c89efefda2f
Gabrielmbl <gabrielmiranda7822@gmail.com>
2024-07-24 10:44:50 -0400

    Formatted total time played for stats.

--- app/models/leaderboard.rb
+++ app/models/leaderboard.rb
@@ -9,4 +9,8 @@ class Leaderboard < ApplicationRecord
   def self.ransackable_attributes(auth_object = nil)
     %w[user_name total_games_joined total_games_completed total_time_played wins losses win_rate]
   end
+
+  def formatted_total_time_played
+    Time.at(total_time_played).utc.strftime('%H:%M:%S')
+  end
 end
